b2166e6659e3cf2f2880412c50490c8881652fae
Pablo Antipan <antipan.pablo@gmail.com>
2025-02-04 21:41:52 -0300

    [ADD] update gateway service for avoiding routes repository

---
+++ config/routes.go
@@ -0,0 +1,34 @@
+package config
+
+type Route struct {
+	ServiceName string
+	Path        string
+	TargetURL   string
+}
+
+type RouterConfig struct {
+	routes map[string]Route
+}
+
+func NewRouterConfig() *RouterConfig {
+	return &RouterConfig{
+		routes: map[string]Route{
+			"/api/v1/players": {
+				ServiceName: "players",
+				Path:        "/api/v1/players",
+				TargetURL:   "http://localhost:8085",
+			},
+			// Easy to add new routes
+			"/api/v1/users": {
+				ServiceName: "users",
+				Path:        "/api/v1/users",
+				TargetURL:   "http://localhost:8086",
+			},
+		},
+	}
+}
+
+func (rc *RouterConfig) GetRoute(path string) (Route, bool) {
+	route, exists := rc.routes[path]
+	return route, exists
+}
--- internal/service/gateway_service.go
+++ internal/service/gateway_service.go
@@ -1,14 +1,17 @@
 package service
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/pabloantipan/go-api-gateway-poc/config"
 	"github.com/pabloantipan/go-api-gateway-poc/internal/data/repository"
 	"github.com/pabloantipan/go-api-gateway-poc/internal/infrastructure/proxy"
 )
 
 type gatewayService struct {
 	routeRepo    repository.RouteRepository
+	routerConfig *config.RouterConfig
 	proxyFactory *proxy.ProxyFactory
 }
 
@@ -20,6 +23,21 @@ func NewGatewayService(repo repository.RouteRepository) GatewayService {
 }
 
 func (s *gatewayService) ProxyRequest(w http.ResponseWriter, r *http.Request) error {
+	route, exists := s.routerConfig.GetRoute(r.URL.Path)
+	if !exists {
+		return fmt.Errorf("no route found")
+	}
+
+	proxy, err := s.proxyFactory.GetProxy(route.TargetURL)
+	if err != nil {
+		return err
+	}
+
+	proxy.ServeHTTP(w, r)
+	return nil
+}
+
+func (s *gatewayService) ProxyRequestFromRepo(w http.ResponseWriter, r *http.Request) error {
 	route, err := s.routeRepo.GetRoute(r.URL.Path)
 	if err != nil {
 		return err
